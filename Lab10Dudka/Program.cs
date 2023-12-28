daf1227f5d7aeb150beca8163d835a2320b2fa4e
B3dRandoms <bdrandoms0@gmail.com>
2023-12-28 02:24:37 +0200

    Initial commit

---
+++ Lab10Dudka/Program.cs
@@ -0,0 +1,52 @@
+﻿class Parallelepiped
+{
+    protected double length;
+    protected double width;
+    protected double height;
+
+    public Parallelepiped(double length, double width, double height)
+    {
+        this.length = length;
+        this.width = width;
+        this.height = height;
+    }
+
+    public double GetVolume()
+    {
+        return length * width * height;
+    }
+}
+
+class MetalBar : Parallelepiped
+{
+    private double density;
+
+    public MetalBar(double length, double width, double height, double density) : base(length, width, height)
+    {
+        this.density = density;
+    }
+
+    public double GetWeight()
+    {
+        return GetVolume() * density;
+    }
+
+    public string GetInfo()
+    {
+        return $"Dimensions: {length} x {width} x {height}, Density: {density}, Weight: {GetWeight()}";
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Создаем объект класса MetalBar
+        MetalBar metalBar = new MetalBar(10, 5, 2, 7.8);
+
+        // Выводим информацию о металлическом брусе
+        Console.WriteLine(metalBar.GetInfo());
+
+        Console.ReadLine();
+    }
+}
